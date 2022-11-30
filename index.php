1adcbf2cb91295124ed2c9dcbae9c5b8d9292374
Mahmood-h <302741395@student.rocmondriaan.nl>
2022-11-30 15:53:13 +0100

    test

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=cijfersysteem",
+    "root", "");
+$query = $db->prepare("SELECT * FROM leerlingen");
+$query->execute();
+$leerlingen = $query->fetchAll(PDO::FETCH_ASSOC);
+$teller = 1;
+echo "<table>";
+echo "<tr>";
+echo "<td> Nummer </td>";
+echo "<td> Leerling </td>";
+echo "<td> Vak </td>";
+echo "<td> Cijfer </td>";
+echo "</tr>";
+foreach ($leerlingen as $leerling) {
+
+    echo "<tr>";
+    echo "<td>" . $teller . "</td>";
+    echo "<td>" . $leerling['leerling'] . "</td>";
+    echo "<td>" . $leerling['vak'] . "</td>";
+    echo "<td>" . $leerling['cijfer'] . "</td>";
+    echo "<td> <a href='update.php?id=". $leerling["id"]."'>Update</a>";
+    echo "<td> <a href='delete.php?id=". $leerling["id"]."'>Delete</a>";
+    echo "</tr>";
+    $teller++;
+}
+echo "</table>"
+?>
+<a href="insert.php">Insert</a>
+<style>
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+
+</style>
+</html>
